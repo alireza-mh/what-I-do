8ac364c7dbe3dcaefacf49bc5e7f6274c745b0d7
Alireza <alireza.mh@gmail.com>
2020-09-25 20:14:07 +0330

    feat: add github type and alias src

--- src/components/RepoCard.svelte
+++ src/components/RepoCard.svelte
@@ -1,3 +1,27 @@
 <script lang="ts">
-
+  import { GithubItem } from "src/routes/about.svelte";
+  export let cardInfo: GithubItem;
+  console.log(cardInfo);
 </script>
+
+<style lang="scss">
+  .tb-repo-card {
+    border: 1px solid #e1e4e8;
+    border-radius: 6px;
+    padding: 16px;
+  }
+  .repo-card-title {
+    a {
+      color: #0366d6;
+      text-decoration: none;
+      font-weight: 500;
+    }
+  }
+</style>
+
+<div class="tb-repo-card">
+  <div class="repo-card-title">
+    <img src="/assets/book.svg" alt="repo book icon" />
+    <a class="repo-card-link" href={cardInfo.url}>{cardInfo.name}</a>
+  </div>
+</div>

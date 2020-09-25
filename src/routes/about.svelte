17769e001a3de0972c76627a253ba7ad7b768bac
Alireza <alireza.mh@gmail.com>
2020-09-25 20:14:07 +0330

    feat: add github type and alias src

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,46 +7,52 @@
       });
   }
   export type GithubItem = {
-	node:{
-		descriptionHTML: string,
-		stargazers:{
-			totalCount: number;
-		},
-		name: string,
-		updatedAt: string,
-		url: string,
-	}
-  } 
+    descriptionHTML: string;
+    stargazers: {
+      totalCount: number;
+    };
+    name: string;
+    primarylanguage: {
+      name: string;
+      color: string;
+    };
+    updatedAt: string;
+    url: string;
+  };
+
+  export type GithubItemNode = {
+    node: GithubItem;
+  };
 </script>
 
-  
 <script lang="ts">
-import RepoCard from 'src/components/RepoCard.svelte';
+  import RepoCard from "src/components/RepoCard.svelte";
 
   export let githubs: IGithub;
-  export let githubDatas: GithubItem[] = githubs.data.user.pinnedItems.edges; 
-  
- 
-  interface IGithub{
-	  data: {
-		  user:{
-			  pinnedItems:{
-				  edges: Array<GithubItem>
-				  totalCount: number;
-			  }
-		  }
-	  }
+  export let githubDatas: GithubItemNode[] = githubs.data.user.pinnedItems.edges;
+
+  interface IGithub {
+    data: {
+      user: {
+        pinnedItems: {
+          edges: Array<GithubItemNode>;
+          totalCount: number;
+        };
+      };
+    };
   }
 </script>
 
 <svelte:head>
-  <title>About Me</title>
+	<title>About Me</title>
 </svelte:head>
 <p class="justify-text">
   I'm FrontEnd developer insterst with performance and developer exprience
   problems. I'm also like contributing to open source projects. Here I share my
   thoughts and expriences hope you find it useful
 </p>
-<RepoCard/>
+
+
 {#each githubDatas as githubData}
+  <RepoCard cardInfo={githubData.node} />
 {/each}

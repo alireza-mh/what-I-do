277ca1ad788729db95d0f8bac22a17de4c2785fb
Alireza <alireza.mh@gmail.com>
2020-09-25 20:14:07 +0330

    feat: add github type and alias src

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,52 @@
-<svelte:head>
-	<title>About Me</title>
-</svelte:head>
+<script context="module" lang="ts">
+  export function preload({ params, query }) {
+    return this.fetch(`/githubapi.json`)
+      .then((r) => r.json())
+      .then((githubs) => {
+        return { githubs };
+      });
+  }
+  export type GithubItem = {
+	node:{
+		descriptionHTML: string,
+		stargazers:{
+			totalCount: number;
+		},
+		name: string,
+		updatedAt: string,
+		url: string,
+	}
+  } 
+</script>
 
-<p>I'm FrontEnd developer insterst with performance and developer exprience problems. I'm also like contributing to open source projects. Here I share my thoughts and expriences hope you find it useful</p>
+  
+<script lang="ts">
+import RepoCard from 'src/components/RepoCard.svelte';
+
+  export let githubs: IGithub;
+  export let githubDatas: GithubItem[] = githubs.data.user.pinnedItems.edges; 
+  
+ 
+  interface IGithub{
+	  data: {
+		  user:{
+			  pinnedItems:{
+				  edges: Array<GithubItem>
+				  totalCount: number;
+			  }
+		  }
+	  }
+  }
+</script>
+
+<svelte:head>
+  <title>About Me</title>
+</svelte:head>
+<p class="justify-text">
+  I'm FrontEnd developer insterst with performance and developer exprience
+  problems. I'm also like contributing to open source projects. Here I share my
+  thoughts and expriences hope you find it useful
+</p>
+<RepoCard/>
+{#each githubDatas as githubData}
+{/each}
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const Dotenv = require("dotenv-webpack");
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
-const alias = { svelte: path.resolve("node_modules", "svelte") };
+const alias = { svelte: path.resolve("node_modules", "svelte"), src: path.resolve(__dirname, 'src'),  };
 const extensions = [".mjs", ".js", ".json", ".svelte", ".html"];
 const mainFields = ["svelte", "module", "browser", "main"];
 
